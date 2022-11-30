9c302cb6cced0205bb4b07e3ff9622fcbd6f8df6
vladzhuganov <vladzhuganov@gmail.com>
2022-11-30 13:31:58 -0500

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("First commit");
         System.out.println("Second Commit");
+        System.out.println("Third commit");
 
 
            
